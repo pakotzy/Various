fb2c9d004b3be724e4b824f3ae0378a9658c8510
Pavlo Kotelnytskyi <pakotzy@gmail.com>
2018-05-15 16:18:11 +0300

    Structure + Views

---
+++ Blog/src/test/java/com/pakotzy/various/blog/BlogMVCApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pakotzy.various.blog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogMVCApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
